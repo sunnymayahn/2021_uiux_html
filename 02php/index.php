a260716c69a96b06de38b46eddb7d8cbe54a82d7
sunnymayahn <sunnysunnytown@gamil.com>
2021-09-06 16:53:27 +0900

    906

---
+++ 02php/index.php
@@ -0,0 +1,11 @@
+<?include "header.php"?>
+<div class="visual">
+subpage
+</div>
+<div class="contents clearfix">
+    <section class="section"></section>
+    <aside class="aside">aside</aside>
+
+</div>
+
+<?include "footer.php"?>
---
+++ 02php/sub01.php
@@ -0,0 +1,6 @@
+<?include "header.php"?>
+<div class="subVisual">
+    <h3>sub01</h3></div>
+<div class="contents"></div>
+
+<?include "footer.php"?>
